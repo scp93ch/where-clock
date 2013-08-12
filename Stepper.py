cc6a83b73b6397fdd317a02dca8140af2745e13d
Stephen C Phillips <steve@scphillips.com>
2013-08-12 23:02:20 +0100

    Adds full step drive mode capability, improves documentation.

--- Stepper.py
+++ Stepper.py
@@ -11,12 +11,23 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 class Motor(object):
-    def __init__(self, pins):
+    def __init__(self, pins, mode=3):
+        """Initialise the motor object.
+
+        pins -- a list of 4 integers referring to the GPIO pins that the IN1, IN2
+                IN3 and IN4 pins of the ULN2003 board are wired to
+        mode -- the stepping mode to use:
+                1: wave drive (not yet implemented)
+                2: full step drive
+                3: half step drive (default)
+
+        """
         self.P1 = pins[0]
         self.P2 = pins[1]
         self.P3 = pins[2]
         self.P4 = pins[3]
-        self.deg_per_step = 5.625 / 64
+        self.mode = mode
+        self.deg_per_step = 5.625 / 64  # for half-step drive (mode 3)
         self.steps_per_rev = int(360 / self.deg_per_step)  # 4096
         self.step_angle = 0  # Assume the way it is pointing is zero degrees
         for p in pins:
@@ -42,17 +53,58 @@ class Motor(object):
         if steps > self.steps_per_rev / 2:
             steps -= self.steps_per_rev
             print "moving " + `steps` + " steps"
-            self._move_acw(-steps / 8)
+            if self.mode == 2:
+                self._move_acw_2(-steps / 8)
+            else:
+                self._move_acw_3(-steps / 8)
         else:
             print "moving " + `steps` + " steps"
-            self._move_cw(steps / 8)
+            if self.mode == 2:
+                self._move_cw_2(steps / 8)
+            else:
+                self._move_cw_3(steps / 8)
         self.step_angle = target_step_angle
-        
-    def _move_acw(self, big_steps):
+
+    def __clear(self):
         GPIO.output(self.P1, 0)
         GPIO.output(self.P2, 0)
         GPIO.output(self.P3, 0)
         GPIO.output(self.P4, 0)
+
+    def _move_acw_2(self, big_steps):
+        self.__clear()
+        for i in range(big_steps):
+            GPIO.output(self.P3, 0)
+            GPIO.output(self.P1, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P2, 0)
+            GPIO.output(self.P4, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P1, 0)
+            GPIO.output(self.P3, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P4, 0)
+            GPIO.output(self.P2, 1)
+            sleep(self._T * 2)
+    
+    def _move_cw_2(self, big_steps):
+        self.__clear()
+        for i in range(big_steps):
+            GPIO.output(self.P4, 0)
+            GPIO.output(self.P2, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P1, 0)
+            GPIO.output(self.P3, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P2, 0)
+            GPIO.output(self.P4, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P3, 0)
+            GPIO.output(self.P1, 1)
+            sleep(self._T * 2)
+    
+    def _move_acw_3(self, big_steps):
+        self.__clear()
         for i in range(big_steps):
             GPIO.output(self.P1, 0)
             sleep(self._T)
@@ -71,11 +123,8 @@ class Motor(object):
             GPIO.output(self.P4, 1)
             sleep(self._T) 
 
-    def _move_cw(self, big_steps):
-        GPIO.output(self.P1, 0)
-        GPIO.output(self.P2, 0)
-        GPIO.output(self.P3, 0)
-        GPIO.output(self.P4, 0)        
+    def _move_cw_3(self, big_steps):
+        self.__clear()
         for i in range(big_steps):            
             GPIO.output(self.P3, 0)
             sleep(self._T) 
@@ -104,9 +153,8 @@ if __name__ == "__main__":
     sleep(1)
     m.move_to(0)
     sleep(1)
-    m.move_to(-90)
-    sleep(1)
-    m.move_to(-180)
+    m.mode = 2
+    m.move_to(90)
     sleep(1)
     m.move_to(0)
     GPIO.cleanup()
